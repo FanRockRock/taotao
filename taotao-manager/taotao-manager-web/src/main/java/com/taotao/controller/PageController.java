9d196d5f42c4ff1f6ea1b51b5a0225ddb255ee07
wangfan <1905526661@qq.com>
2017-02-21 10:13:17 +0800

    首页

---
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/PageController.java
@@ -0,0 +1,17 @@
+package com.taotao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/** 
+ * 类说明 
+ * @author wangfan 
+ * @version 创建时间：2017年2月21日 上午9:54:40 
+ */
+@Controller
+public class PageController {
+	@RequestMapping("/")
+	public String showIndex() {
+		return "index";
+	}
+}
