7db9da5ab4b3475d578249d47994638db0471eab
wangfan <1905526661@qq.com>
2017-03-06 09:36:46 +0800

    commit web 111

--- taotao-manager/taotao-manager-web/src/test/java/com/taotao/fastdfs/FastdfsTest.java
+++ taotao-manager/taotao-manager-web/src/test/java/com/taotao/fastdfs/FastdfsTest.java
@@ -1,23 +1,41 @@
 package com.taotao.fastdfs;
 
 import org.csource.common.MyException;
-import org.csource.fastdfs.ClientGlobal;
+import org.csource.fastdfs.*;
+import org.junit.Test;
+
 import java.io.IOException;
 
 /**
  * Created by Administrator on 2017/3/5.
  */
+
 public class FastdfsTest {
+    @Test
     public void testUpload() throws IOException, MyException {
-      //  1、把FastDFS提供的jar包添加到工程中
-      //  2、初始化全局配置。加载一个配置文件。
+        //  1、把FastDFS提供的jar包添加到工程中
+        //  2、初始化全局配置。加载一个配置文件。
         ClientGlobal.init("E:\\IdeaProjects\\taotao\\taotao-manager\\taotao-manager-web\\src\\main\\resources\\properties\\client.conf");
-       // 3、创建一个TrackerClient对象。
-
-      //  4、创建一个TrackerServer对象。
-      //  5、声明一个StorageServer对象，null。
-      //  6、获得StorageClient对象。
-      //  7、直接调用StorageClient对象方法上传文件即可。
-
+        //  3、创建一个TrackerClient对象。
+        TrackerClient trackerClient = new TrackerClient();
+        //  4、创建一个TrackerServer对象。
+        TrackerServer trackerServer = trackerClient.getConnection();
+        //  5、声明一个StorageServer对象，null。
+        StorageServer storageServer = null;
+        //  6、获得StorageClient对象。
+        StorageClient storageClient = new StorageClient(trackerServer, storageServer);
+        //  7、直接调用StorageClient对象方法上传文件即可。
+        String[] strings = storageClient.upload_file("F:\\zhangyuTV.jpg", "jpg", null);
+        for (String s : strings) {
+            System.out.println(s);
+        }
     }
+
+    @Test
+    public void testFastDfsClient() throws Exception {
+        FastDFSClient client = new FastDFSClient("E:\\IdeaProjects\\taotao\\taotao-manager\\taotao-manager-web\\src\\main\\resources\\properties\\client.conf");
+        String uploadFile = client.uploadFile("F:\\zhangyuTV.jpg", "jpg");
+        System.out.println(uploadFile);
+    }
+
 }
